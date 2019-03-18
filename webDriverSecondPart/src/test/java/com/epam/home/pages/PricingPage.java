83a85bf368897b9b10c64082b6556c7b8eba72a6
Alexandr Ludanik <sysrq0884@gmail.com>
2019-03-18 12:05:03 -0700

    add wait to calculator button

--- webDriverSecondPart/src/test/java/com/epam/home/pages/PricingPage.java
+++ webDriverSecondPart/src/test/java/com/epam/home/pages/PricingPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class PricingPage extends AbstractPage {
 
@@ -18,6 +19,7 @@ public class PricingPage extends AbstractPage {
 
 
     public CalculatorPage clickCalculatorButton() {
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(calculatorButton));
         calculatorButton.click();
         logger.info("Click on link 'Calculators'");
         return new CalculatorPage(driver);
